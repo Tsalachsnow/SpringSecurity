b1e1d1b7eeaa468394c50417eb48107a5a6dbf1d
TsalachSnow <prosper.amalaha@decagon.dev>
2022-01-04 22:57:59 +0100

    switching from database authentication to JWT and adding the JWT username and password authentication filter

--- src/main/java/com/example/springsecurity1/security/APPSecurityConfig.java
+++ src/main/java/com/example/springsecurity1/security/APPSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.springsecurity1.security;
 
+import com.example.springsecurity1.JWT.JWTUsernameAndPasswordAuthenticationFilter;
 import com.example.springsecurity1.auth.ApplicationUserService;
 import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;;
 import static com.example.springsecurity1.security.ApplicationUserRole.*;
@@ -35,6 +37,10 @@ public class APPSecurityConfig extends WebSecurityConfigurerAdapter {
         http    //.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                 //.and()
                 .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilter(new JWTUsernameAndPasswordAuthenticationFilter(authenticationManager()))
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*")
                 .permitAll()
@@ -45,28 +51,29 @@ public class APPSecurityConfig extends WebSecurityConfigurerAdapter {
 //                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAnyAuthority(COURSE_WRITE.getPermission())
 //                .antMatchers(HttpMethod.GET,"/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
-                .authenticated()
-                .and()
-                //.httpBasic();
-                .formLogin().loginPage("/login")
-                .permitAll()
-                .defaultSuccessUrl("/courses", true)
-                .passwordParameter("password")
-                .usernameParameter("username")
-                .and()
-                .rememberMe() //SessionId last only for 30 minutes of inactivity so a rememberMe() feature can be added
-                 // defaults to 2 weeks
-                 .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))/*extending the default of remember me()
-                 session to anything you want*/
-                .key("somethingsecured").rememberMeParameter("remember-me")
-                .and()
-                .logout()//adding logout in spring security
-                .logoutUrl("/logout")
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
-                .clearAuthentication(true)
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID", "remember-me")
-                .logoutSuccessUrl("/login");
+                .authenticated(); /*switching to jwt we don't need the form-based and data-based authentication processes below
+                                    So we comment them out*/
+//                .and()
+//              //.httpBasic();
+//                .formLogin().loginPage("/login")
+//                .permitAll()
+//                .defaultSuccessUrl("/courses", true)
+//                .passwordParameter("password")
+//                .usernameParameter("username")
+//                .and()
+//                .rememberMe() //SessionId last only for 30 minutes of inactivity so a rememberMe() feature can be added
+//                 // defaults to 2 weeks
+//                 .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))/*extending the default of remember me()
+//                 session to anything you want*/
+//                .key("somethingsecured").rememberMeParameter("remember-me")
+//                .and()
+//                .logout()//adding logout in spring security
+//                .logoutUrl("/logout")
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
+//                .clearAuthentication(true)
+//                .invalidateHttpSession(true)
+//                .deleteCookies("JSESSIONID", "remember-me")
+//                .logoutSuccessUrl("/login");
 
     }
 
